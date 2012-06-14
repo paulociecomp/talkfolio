41e6d71af9bdd906d19229d63aa9c22875a3181e
Felipe Iketani <felipeiketani@gmail.com>
2012-06-14 02:19:29 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Talkfolio::Application.config.secret_token = '36c58e1dbe063b44b398cad742a2053ffc0a8330afc89cdf1865a8e45f3db1742dac9de11eee06ccd441fbf720d28f2d892f928d81849023e163291671448182'
