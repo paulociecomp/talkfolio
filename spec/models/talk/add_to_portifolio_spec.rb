c2017a8ec4744b8d5d03403106404e14e850c1b5
Felipe Iketani <felipeiketani@gmail.com>
2012-06-14 02:47:58 -0300

    improved add talk to portfolio spec

--- spec/models/talk/add_to_portifolio_spec.rb
+++ spec/models/talk/add_to_portifolio_spec.rb
@@ -5,8 +5,7 @@ describe Talk, "add to portfolio" do
   subject { Talk.new(:title => "Hello", :description => "Hello world") }
 
   it "saves the talk in the data base" do
-    
     subject.add_to_portfolio
-    subject.id.should_not be_nil
+    subject.should be_persisted
   end
 end
